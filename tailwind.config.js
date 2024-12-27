a87312d44fad2ae3272a0d939fc137758ade3624
Cayo Silva <cayo.silva096@gmail.com>
2024-12-27 18:20:19 -0300

    First commit containing the Revhin Landing Page

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        revhinGreen: '#6A8E7F',
+        revhinBlack: '#191716',
+        revhinRed: '#9F2042',
+        revhinWhite: '#F8E5EE',
+        revhinBlue: '#072ac8',
+        revhinDarkBlue: '#1e293b'
+      },
+      fontFamily: {
+        kodchasan: ['Kodchasan', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
