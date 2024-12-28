d0e7b21006e88e4671d99e39ca3e7bf1e83e6c23
Cayo Silva <cayo.silva096@gmail.com>
2024-12-28 10:56:38 -0300

    fixed the padding in mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: [
+    "./dist/*.{html,js}",
+    "./index.html"
+  ],
   theme: {
     extend: {
       colors:{
